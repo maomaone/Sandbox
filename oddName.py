5a558b97b0ad170c827f5b99edc0885e5e72a03f
Annabelle <annabelle.lin@my.jcu.edu.au>
2018-03-28 16:01:25 +0800

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,10 @@
 """annabelle"""
+
+while True:
+    name = str(input("Please enter your name:"))
+    if name == "":
+        print("Invalid name")
+    else:
+        print(name[1::2])
+        break
+        
